3d7df7e9bc06ce4f3bcb80d3bb169faa696b0990
Bartek Ogryczak <bartek.ogryczak@chime.com>
2022-12-03 16:06:55 -0800

    test string input

--- advent/util.py
+++ advent/util.py
@@ -7,24 +7,31 @@ DEFAULT_INPUT_FILE = 'input.txt'
 def get_input_path() -> str:
     return Path(__main__.__file__).with_name(DEFAULT_INPUT_FILE)
 
-def get_input_lines():
-    with open(get_input_path()) as f:
+def get_input_file(_test_str=None):
+    if _test_str:
+        return io.StringIO(_test_str)
+    else:
+        return open(get_input_path())
+
+
+def get_input_lines(_test_str=None):
+    with get_input_file(_test_str) as f:
         while True:
             try:
                 yield next(f).strip()
             except StopIteration:
                 break
 
-def get_input_chunks_by_size(size=3):
-    with open(get_input_path()) as f:
+def get_input_chunks_by_size(size=3, _test_str=None):
+    with get_input_file(_test_str) as f:
         while True:
             try:
                 yield [next(f).strip() for _ in range(size)]
             except StopIteration:
                 break
 
-def get_input_chunks_by_separator(separator=''):
-    with open(get_input_path()) as f:
+def get_input_chunks_by_separator(separator='', _test_str=None):
+    with get_input_file(_test_str) as f:
         chunk = []
         for line in f:
             line = line.strip()
