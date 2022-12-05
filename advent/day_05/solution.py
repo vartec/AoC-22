ef5b8b960f495ff2a4d4f957766850ce78ca5d52
Bartek Ogryczak <bartek.ogryczak@chime.com>
2022-12-05 00:18:03 -0800

    day 5

---
+++ advent/day_05/solution.py
@@ -0,0 +1,39 @@
+from copy import deepcopy
+from advent.util import * # noqa
+
+def parse_stacks(stack_lines: list[str]) -> dict[str: list[str]]:
+    stacks = {i.strip():list() for i in stack_lines.pop().split()}
+    for line in reversed(stack_lines):
+        for (key, chunk) in zip(stacks.keys(), zip(*[iter(line)] * 4)):
+            crate = chunk[1]
+            if crate != ' ':
+                stacks[key].append(crate)
+    return stacks
+
+def parse_move(line: str) -> tuple:
+    parts = line.split()
+    return int(parts[1]), parts[3], parts[5]
+
+
+stack_lines = []
+with get_input_file() as f:
+    for line in f:
+        if line == '\n':
+            break
+        stack_lines.append(line)
+
+    stacks_1 = parse_stacks(stack_lines)
+    stacks_2 = deepcopy(stacks_1)
+
+    for line in f:
+        how_many, move_from, move_to = parse_move(line)
+        stacks_1[move_to].extend(reversed(stacks_1[move_from][-1*how_many:]))
+        del stacks_1[move_from][-1*how_many:]
+        stacks_2[move_to].extend(stacks_2[move_from][-1*how_many:])
+        del stacks_2[move_from][-1*how_many:]
+
+result_1 = ''.join(s.pop() for s in stacks_1.values())  # it's an ordered dict, so it's in the in right order
+result_2 = ''.join(s.pop() for s in stacks_2.values())
+
+print(f'part 1: {result_1}')
+print(f'part 2: {result_2}')
